222f27c728497964d68a179e0d4868514bac1d69
coden-arfat <52942762+coden-arfat@users.noreply.github.com>
2022-07-13 11:53:01 +0600

    some work with strings
    Signed-off-by: coden-arfat <52942762+coden-arfat@users.noreply.github.com>

---
+++ sprint1/sub_strings.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std ;
+int main(){
+  string  str ="hello world";
+  // note : subtr(begining index, ending index)
+  cout<<str.substr(1,6)<<endl ; //ello w
+
+  
+    
+  return 0 ;
+} 
